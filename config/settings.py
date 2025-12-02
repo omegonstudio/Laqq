2de9dfa778f4d3b9f8a7bec7e218b266218d39fa
malenarmr <maalena456@gmail.com>
2025-12-02 15:28:29 -0300

    Fix cors

--- config/settings.py
+++ config/settings.py
@@ -208,6 +208,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8080",
+    "http://localhost:8081"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
