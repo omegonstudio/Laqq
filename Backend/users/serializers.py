1bdb132cb59a4bb946e947f0742131cb3cdee009
MatiasMirarchi <matiasmirarchi99@gmail.com>
2026-01-29 15:15:05 -0300

    add user-type in the endpoint users/token

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import UserType, UserState
 
+# Simple JWT
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 User = get_user_model()
 
 class UserTypeSerializer(serializers.ModelSerializer):
@@ -47,3 +50,43 @@ class UserCreateSerializer(UserSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+#
+# Custom Token serializer to return user_type alongside tokens
+#
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Extiende TokenObtainPairSerializer para:
+    - Añadir claims ligeros en el token (user_type_id, user_type_name)
+    - Incluir en la respuesta JSON un campo `user_type` con {id, name} para que el frontend lo consuma fácilmente.
+    """
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Añadir claims si el usuario tiene user_type
+        if getattr(user, 'user_type', None):
+            # user_type.id en tu modelo es un CharField primary key
+            token['user_type_id'] = str(user.user_type.id)
+            token['user_type_name'] = user.user_type.name
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        user = self.user
+
+        # Añadir objeto user_type a la respuesta
+        if getattr(user, 'user_type', None):
+            data['user_type'] = {
+                'id': user.user_type.id,
+                'name': user.user_type.name
+            }
+        else:
+            data['user_type'] = None
+
+        # opcional: exponer flags útiles (si quieres)
+        data['is_staff'] = user.is_staff
+        data['is_superuser'] = user.is_superuser
+
+        return data
--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserTypeViewSet, UserStateViewSet, UserViewSet
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import UserTypeViewSet, UserStateViewSet, UserViewSet, CustomTokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'list', UserViewSet, basename='user')
@@ -9,7 +9,7 @@ router.register(r'types', UserTypeViewSet)
 router.register(r'states', UserStateViewSet)
 
 urlpatterns = [
-    # JWT Token endpoints
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT Token endpoints (usamos la view custom para token/)
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
